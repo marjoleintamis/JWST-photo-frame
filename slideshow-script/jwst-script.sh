d57f3d2f6a472e22f62e33b84982ae323a828168
Marjolein Tamis <80948475+marjoleintamis@users.noreply.github.com>
2025-08-03 15:51:39 +0200

    Update jwst-script.sh

--- slideshow-script/jwst-script.sh
+++ slideshow-script/jwst-script.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-while true
-do
-	fbi -d /dev/fb0  -T 1 -noverbose -a -t 300 --once /path/to/images/*
-done
+# Options are with large font, 300 seconds to move to the next slide. There's an extra /* in there so directories will get picked up. If you don't want that, leave that out.
+sudo fbi -d /dev/fb0 -T 1 -f monospace:size=32 -a -t 300 /path/to/pictures/*/*
